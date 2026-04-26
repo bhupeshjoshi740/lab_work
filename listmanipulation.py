f49027f53db7f5450a8bb288be1bb9dfd10453c7
Bhupesh joshi <bhupesh.joshi054@gmail.com>
2026-04-26 17:09:10 +0530

    Add files via upload

---
+++ listmanipulation.py
@@ -0,0 +1,12 @@
+#creating first list list
+list1 = [45,47,89,34,56]
+#creating second list list2
+list2 = [23,56,78,90,12]
+#to concatenation two lists we can use + operator
+list3= list1 + list2
+print("concatenated list is :",list3)
+#using multiplication operator to repeat list1 five times
+list4 = list1 * 5
+print("list1 repeated five times is : ",list4)
+
+#------------------------------
